e450debf1d1d5d787ee1ce5ce78bd2f943feb353
Dmitry <dmstk13@gmail.com>
2023-07-18 11:09:11 +0300

    Commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,16 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
+
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carService) {
+    public CarController(CarService carService) {
         this.carService = carService;
     }
+
 
     @GetMapping("/cars")
     public String something(@RequestParam(defaultValue = "5") int count, Model model) {
