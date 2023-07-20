8dd02062c88b71054b825e9bd71934bf0d070e47
Dmitry <dmstk13@gmail.com>
2023-07-20 12:40:20 +0300

    Commit

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -3,51 +3,108 @@ package web.dao;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Repository
 public class UserDAOImpl implements UserDAO {
     private static int PEOPLE_COUNT;
-    private final List<User> users;
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "951!Dmstk13";
 
-    {
-        users = new ArrayList<>();
-        users.add(new User(++PEOPLE_COUNT, "Bob", "Boba"));
-        users.add(new User(++PEOPLE_COUNT, "Tom", "Goth"));
-        users.add(new User(++PEOPLE_COUNT, "John", "Over"));
-        users.add(new User(++PEOPLE_COUNT, "Bill", "Nick"));
-        users.add(new User(++PEOPLE_COUNT, "Sam", "Dann"));
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public List<User> users() {
+        List<User> users = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setName(resultSet.getString("surname"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return users;
     }
 
     @Override
     public void add(User user) {
-        user.setId(++PEOPLE_COUNT);
-        users.add(user);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users VALUES(14, ?, ?)");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getSurname());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void delete(int id) {
-        users.removeIf(p -> p.getId() == id);
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void edit(int id, User updatedUser) {
-        User userToBeUpdated = show(id);
-        userToBeUpdated.setName(updatedUser.getName());
-        userToBeUpdated.setSurname(updatedUser.getSurname());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET name=?, surname=? WHERE id=?");
+            preparedStatement.setString(1, updatedUser.getName());
+            preparedStatement.setString(2, updatedUser.getSurname());
+            preparedStatement.setInt(3, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public User show(int id) {
-        return users.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        User user = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT FROM users WHERE id=?");
+            preparedStatement.setInt(1, id);
+           ResultSet resultSet = preparedStatement.executeQuery();
+           resultSet.next();
+           user = new User();
+           user.setId(resultSet.getInt("id"));
+           user.setName(resultSet.getString("name"));
+           user.setSurname(resultSet.getString("surname"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
