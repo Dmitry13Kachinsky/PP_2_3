b0564b9ce68ecfd004a3aaa180a90e84d89d4b01
Dmitry <dmstk13@gmail.com>
2023-07-20 13:47:59 +0300

    Commit

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -1,110 +1,45 @@
 package web.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Repository
 public class UserDAOImpl implements UserDAO {
-    private static int PEOPLE_COUNT;
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "951!Dmstk13";
+    private final JdbcTemplate jdbcTemplate;
 
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+    @Autowired
+    public UserDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public List<User> users() {
-        List<User> users = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "SELECT * FROM users";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setName(resultSet.getString("surname"));
-                users.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return users;
+        return jdbcTemplate.query("SELECT * FROM users", new BeanPropertyRowMapper<>(User.class));
     }
 
     @Override
     public void add(User user) {
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users VALUES(14, ?, ?)");
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getSurname());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        jdbcTemplate.update("INSERT INTO users VALUES (15, ?, ?)", user.getName(), user.getSurname());
     }
 
     @Override
     public void delete(int id) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id=?");
-            preparedStatement.setInt(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        jdbcTemplate.update("DELETE FROM users WHERE id=?", id);
     }
 
     @Override
     public void edit(int id, User updatedUser) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET name=?, surname=? WHERE id=?");
-            preparedStatement.setString(1, updatedUser.getName());
-            preparedStatement.setString(2, updatedUser.getSurname());
-            preparedStatement.setInt(3, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        jdbcTemplate.update("UPDATE users SET name=?, surname=? WHERE id=?", updatedUser.getName(), updatedUser.getSurname(), id);
     }
 
     @Override
     public User show(int id) {
-        User user = null;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT FROM users WHERE id=?");
-            preparedStatement.setInt(1, id);
-           ResultSet resultSet = preparedStatement.executeQuery();
-           resultSet.next();
-           user = new User();
-           user.setId(resultSet.getInt("id"));
-           user.setName(resultSet.getString("name"));
-           user.setSurname(resultSet.getString("surname"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return user;
+        return jdbcTemplate.query("SELECT * FROM users WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(User.class)).stream().findAny().orElse(null);
     }
 }
